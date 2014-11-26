0240d400b1bece346a6ec64d1223df57141913a0
Robb Lee <robbmlee@gmail.com>
2014-11-25 19:33:16 -0700

    Some tinkering

--- javascripts/controller.js
+++ javascripts/controller.js
@@ -1,22 +1,31 @@
-function IanGameController(canvasID) {
+function IanGameController(canvasID, centerX, centerY) {
     this.canvas = document.getElementById(canvasID);
-        this.canvas.width=self.innerWidth - 50;
-        this.canvas.height=self.innerHeight - 50;
-        this.canvas.style.border="1px solid #000000";
+    this.canvas.width=self.innerWidth - 20;
+    this.canvas.height=self.innerHeight - 20;
+    this.canvas.style.border="1px solid #000000";
+    this.centerX = centerX || this.canvas.width / 2;
+    this.centerY = centerY || this.canvas.height / 2;
+    this.context = this.canvas.getContext('2d');
 }
 
-IanGameController.prototype.drawScull = function(centerX, centerY) {
-    var context = this.canvas.getContext("2d");
+IanGameController.prototype.drawScull = function() {
+    this.imageObj = new Image();
+    this.imageObj.src = 'reference_scull.png';
 
-    centerX = centerX || this.canvas.width / 2;
-    centerY = centerY || this.canvas.height / 2;
+    this.imageObj.onload = (function() {
+        this.context.drawImage(this.imageObj, this.centerX - 65, this.centerY - 65, 130, 130);
+    }).bind(this);
+}
+
+IanGameController.prototype.drawScull_OLD = function() {
+    var context = this.canvas.getContext("2d");
     
     var width = 130;
     var height = 240;
 
-    var leftBoundry = centerX - width/2;
+    var leftBoundry = this.centerX - width/2;
     var rightBoundry = leftBoundry + width;
-    var topBoundry = centerY - height/2;
+    var topBoundry = this.centerY - height/2;
     var bottomBoundry = topBoundry + height;
     
     // begin custom shape
@@ -61,26 +70,26 @@ IanGameController.prototype.drawScull = function(centerX, centerY) {
     var eyeDiameter = 10;
     var eyeXOffset = 25;
     context.beginPath();
-    context.arc(centerX - eyeXOffset, originY, eyeDiameter, 0, 2 * Math.PI);
+    context.arc(this.centerX - eyeXOffset, originY, eyeDiameter, 0, 2 * Math.PI);
     context.fillStyle = 'black';
     context.fill();
     context.stroke();
 
     // right eye
     context.beginPath();
-    context.arc(centerX + eyeXOffset, originY, eyeDiameter, 0, 2 * Math.PI);
+    context.arc(this.centerX + eyeXOffset, originY, eyeDiameter, 0, 2 * Math.PI);
     context.fillStyle = 'black';
     context.fill();
     context.stroke();
 
     //nose
     context.beginPath();
-    var noseTop = centerY - 15;
+    var noseTop = this.centerY - 15;
     var noseDim = 20;
     var noseBottom = noseTop + noseDim;
-    context.moveTo(centerX, noseTop);
-    context.lineTo(centerX + noseDim/2, noseBottom);
-    context.lineTo(centerX - noseDim/2, noseBottom);
+    context.moveTo(this.centerX, noseTop);
+    context.lineTo(this.centerX + noseDim/2, noseBottom);
+    context.lineTo(this.centerX - noseDim/2, noseBottom);
     context.fillStyle = 'black';
     context.fill();
     context.closePath();
@@ -88,10 +97,10 @@ IanGameController.prototype.drawScull = function(centerX, centerY) {
 
     //mouth
     context.beginPath();
-    var mouthCenterY = centerY + 40;
+    var mouthCenterY = this.centerY + 40;
     var mouthWidth = 40;
     var mouthHeight = 20;
-    var mouthLeft = centerX - mouthWidth/2;
+    var mouthLeft = this.centerX - mouthWidth/2;
     var mouthRight = mouthLeft + mouthWidth;
     var mouthTop = mouthCenterY - mouthHeight/2;
     var mouthBottom = mouthTop + mouthHeight;
@@ -101,19 +110,23 @@ IanGameController.prototype.drawScull = function(centerX, centerY) {
     context.lineTo(mouthRight, mouthCenterY);
 
 
-    context.moveTo(centerX, mouthTop);
-    context.lineTo(centerX, mouthBottom);
+    context.moveTo(this.centerX, mouthTop);
+    context.lineTo(this.centerX, mouthBottom);
 
-    for(var i = mouthLeft + 5; i < centerX; i = i + 5) {
+    for(var i = mouthLeft + 5; i < this.centerX; i = i + 5) {
         context.moveTo(i, mouthTop);
         context.lineTo(i, mouthBottom);
     }
 
-    for(var i = mouthRight - 5; i > centerX; i = i - 5) {
+    for(var i = mouthRight - 5; i > this.centerX; i = i - 5) {
         context.moveTo(i, mouthTop);
         context.lineTo(i, mouthBottom);
     }
 
     context.lineWidth = 2;
     context.stroke();
+}
+
+IanGameController.prototype.drawBones = function(centerX, centerY) {
+    var context = this.canvas.getContext("2d");
 }
